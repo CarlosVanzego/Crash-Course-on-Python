9908f0a7e3915d0927454aba9301d598bb22bc48
Your Name <carlosvanzego96@gmail.com>
2025-03-26 10:05:30 -0500

    calculator.py updates

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
 print(4 + 4)
+# this will print 8
+
+print(8 * 2)
+# this will print 16
+
+print(-2/2)
+# this will print -1.0
+
+print(1/4)
+# this will print 0.25
+
+print(((2025/4) - 24)/2)
+# this will print 241.125
+
+print(2**2)
+# this will print 4
