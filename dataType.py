21009e1d76e6ffb737abb86406a0ed3cb335bdda
Your Name <carlosvanzego96@gmail.com>
2025-03-26 14:03:26 -0500

    dataType.py commit

---
+++ dataType.py
@@ -0,0 +1,11 @@
+print(type("c"))
+
+print(type(4))
+
+print(type(4.4))
+
+# within each print statement above, i'm using the type() function to print the data type of each value passed to it.
+# the output of the above code will be:
+#  <class 'str'>
+# <class 'int'>
+# <class 'float'>
