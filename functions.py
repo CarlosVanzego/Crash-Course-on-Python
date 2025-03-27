6a5612c9fd439ad8a17d2438caf0e4424cc2a4cb
Your Name <carlosvanzego96@gmail.com>
2025-03-26 19:17:14 -0500

    functions.py update

--- functions.py
+++ functions.py
@@ -7,3 +7,20 @@ def to_celsius(x):
 print(to_celsius(44)) # this will print 6.666666666666667
 
 to_celsius(44) 
+
+
+
+
+def greeting(name):
+    print("Welcome, " + name)
+
+greeting("Carlos")  # this will print "Welcome, Carlos"
+greeting("V")  # this will print "Welcome, V" 
+
+
+
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of the " + department)
+
+greeting("Carlos", "IT") # this will print "Welcome , Carlos" You are part of the IT department 
