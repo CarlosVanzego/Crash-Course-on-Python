d9152e904474197b3eaf33a61010fa3afcf11501
Your Name <carlosvanzego96@gmail.com>
2025-03-26 19:00:43 -0500

    expressions.py update

--- expressions.py
+++ expressions.py
@@ -9,3 +9,13 @@ print("a" + "b" + "c")
 
 print("This " + "is " + "pretty " + "cool😎")
 
+
+
+# In the example below, I'm calculating the area of a triangle, and when printint it, I'm adding it to a string.
+# To do this, I'm calling the str() function to convert a number into a string.
+base = 6
+height = 3
+area = (base*height)/2
+
+print("The area of the triangle is: " + str(area))
+
